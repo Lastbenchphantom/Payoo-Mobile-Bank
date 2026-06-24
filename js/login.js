1541b10359aec1abc8fb7a627990a7551d1496cb
Lastbenchphantom <zakariazak13913@gmail.com>
2026-06-24 22:15:07 +0600

    added comments

--- js/login.js
+++ js/login.js
@@ -1,7 +1,10 @@
+//step - 1 set event handler
 document
   .getElementById("btn-login")
   .addEventListener("click", function (event) {
+    //step 2: prevent default behaviour (prevent reloading browser)
     event.preventDefault();
     console.log("login button clicked");
+    //step 3: get the phone number
     const phoneNumber = document.getElementById("phone-number").value;
   });
